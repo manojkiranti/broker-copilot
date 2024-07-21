f6744100e7557f7628530d9a259ff19aee790f5d
manojkiranti <manoj.kiranti@gmail.com>
2024-07-21 18:14:58 +0545

    secure proxy ssl header added

--- copilot/settings.py
+++ copilot/settings.py
@@ -56,6 +56,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 ROOT_URLCONF = 'copilot.urls'
 
 TEMPLATES = [
