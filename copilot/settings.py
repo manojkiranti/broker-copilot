fdaa5226ebaed02e3df4270056689351f964f827
manojkiranti <manoj.kiranti@gmail.com>
2024-07-24 15:15:17 +0545

    removed cron job

--- copilot/settings.py
+++ copilot/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'rest_framework',
-    'django_cron',
+    # 'django_cron',
     'accounts',
     'drf_yasg',
     'utils',
