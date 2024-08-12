db7222041ac83a580ceb61cc81ed844ba69fa936
manojkiranti <manoj.kiranti@gmail.com>
2024-08-12 13:37:52 +0545

    updated log for ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 # Start the SSH service
 service ssh start
 
+# Log the status of the SSH service for debugging
+service ssh status
+
 # Collect static files
 python manage.py collectstatic --noinput
 
